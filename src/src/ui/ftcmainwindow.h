0ce84a8b6990ece848a63004241b9b98e6abfbc9
jkl <jkl@hunesion.com>
2020-06-24 14:09:17 +0900

    copying

--- src/src/ui/ftcmainwindow.h
+++ src/src/ui/ftcmainwindow.h
@@ -12,16 +12,6 @@ FtcMainWindow       *ftc_main_window_new          (FtcApplication *app);
 void                 ftc_main_window_open         (FtcMainWindow *main_window,
                                                          GFile            *file);
 
-void                ftc_main_window_session_logout(FtcMainWindow *main_window, bool is_expired);
-
-void                ftc_main_window_set_detail_send_page(FtcMainWindow *main_window, gpointer page);
-void                ftc_main_window_set_detail_recv_page(FtcMainWindow *main_window, gpointer page);
-void                ftc_main_window_set_detail_send_history_page(FtcMainWindow *main_window, gpointer page);
-gpointer            ftc_main_window_get_detail_send_page(FtcMainWindow *main_window);
-gpointer            ftc_main_window_get_detail_recv_page(FtcMainWindow *main_window);
-gpointer            ftc_main_window_get_detail_send_history_page(FtcMainWindow *main_window);
-
-
 
 
 #endif /* _FTC_UI_MAIN_WINDOW_H_ */
