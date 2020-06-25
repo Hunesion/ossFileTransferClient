c88c8544863b0fc71ffa4e49a082bb1526ef9743
jkl <jkl@hunesion.com>
2020-06-25 14:22:25 +0900

    insert license to source file

--- src/src/ui/ftcmainwindow.h
+++ src/src/ui/ftcmainwindow.h
@@ -1,3 +1,15 @@
+/*
+* Copyright (c) 2020 Hunesion Inc.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; 
+* either version 2.1 of the License, or (at your option) any later version.
+* See the file COPYING included with this distribution for more information.
+* https://github.com/HuneOpenUp/ossFileTransferClient/blob/master/COPYING
+*/
+
+
 #ifndef _FTC_UI_MAIN_WINDOW_H_
 #define _FTC_UI_MAIN_WINDOW_H_
 #include "ftcuicommon.h"
@@ -11,7 +23,6 @@ G_DECLARE_FINAL_TYPE (FtcMainWindow, ftc_main_window, FTC, MAIN_WINDOW, GtkAppli
 FtcMainWindow       *ftc_main_window_new          (FtcApplication *app);
 void                 ftc_main_window_open         (FtcMainWindow *main_window,
                                                          GFile            *file);
-
 
 
 #endif /* _FTC_UI_MAIN_WINDOW_H_ */
