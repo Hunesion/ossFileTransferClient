a0f81ab260ae7813303c0f0e28cf55227a1bc2f6
jaeseokryoo <jaeseokryoo@hunesion.com>
2020-07-22 15:01:51 +0900

    add dbus

---
+++ src/src/dbus/ftcappdbus.h
@@ -0,0 +1,6 @@
+#ifndef _FTC_APP_DBUS_H_
+#define _FTC_APP_DBUS_H_
+#include "playback.h"
+#define FTC_APP_BUS_NAME "org.hunesion.app.bus"
+#define FTC_APP_BUS_OBJ_NAME "/org/hunesion/app/playback"
+#endif 
