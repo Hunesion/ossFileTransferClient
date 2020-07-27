b089c52d84c5370efc5161b88ce34713d8929c5a
dainchoi <dainchoi@hunesion.com>
2020-07-27 14:43:08 +0900

    insert license ftcwebkit

--- src/src/webkit/ftcwebkit.h
+++ src/src/webkit/ftcwebkit.h
@@ -1,3 +1,14 @@
+/*
+* Copyright (c) 2020 Hunesion Inc.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; 
+* either version 2.1 of the License, or (at your option) any later version.
+* See the file COPYING included with this distribution for more information.
+* https://github.com/HuneOpenUp/ossFileTransferClient/blob/master/COPYING
+*/
+
 #ifndef _FTC_WEB_KIT_H_
 #define _FTC_WEB_KIT_H_
 #include <gtk/gtk.h>
