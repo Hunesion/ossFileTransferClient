ee97de73b4585ac70f7b0b3e05d61dc9df72316d
dainchoi <dainchoi@hunesion.com>
2020-07-27 14:42:21 +0900

    insert license ValueObject

--- src/src/core/model/ValueObject.cpp
+++ src/src/core/model/ValueObject.cpp
@@ -1,3 +1,13 @@
+/*
+* Copyright (c) 2020 Hunesion Inc.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; 
+* either version 2.1 of the License, or (at your option) any later version.
+* See the file COPYING included with this distribution for more information.
+* https://github.com/HuneOpenUp/ossFileTransferClient/blob/master/COPYING
+*/
 
 #include "../JsonUtils.h"
 #include "../StringUtils.h"
