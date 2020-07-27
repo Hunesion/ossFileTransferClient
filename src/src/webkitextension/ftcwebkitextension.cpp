a1817d8bd5b862065cf94c6429e08d6e18cead26
dainchoi <dainchoi@hunesion.com>
2020-07-27 14:43:19 +0900

    insert license ftcwebkitextension

--- src/src/webkitextension/ftcwebkitextension.cpp
+++ src/src/webkitextension/ftcwebkitextension.cpp
@@ -1,3 +1,14 @@
+/*
+* Copyright (c) 2020 Hunesion Inc.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; 
+* either version 2.1 of the License, or (at your option) any later version.
+* See the file COPYING included with this distribution for more information.
+* https://github.com/HuneOpenUp/ossFileTransferClient/blob/master/COPYING
+*/
+
 #include <webkit2/webkit-web-extension.h>
 #include <JavaScriptCore/JavaScript.h>
 #include "ftcwebkitextension.h"
