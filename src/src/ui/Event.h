b15fef1ebed35d1928cdf859f21f25c8d98f127f
kjtae <kjtae@hunesion.com>
2020-07-28 14:12:13 +0900

    add EventCallback Manager for UI thread

---
+++ src/src/ui/Event.h
@@ -0,0 +1,13 @@
+#ifndef _FTC_UI_EVENT_H_
+#define _FTC_UI_EVENT_H_
+
+/////////////// STACK EVENT ///////////////
+#define FTC_UI_UPDATE_MAIN_STACK_EVENT          "Ftc::Ui::UpdateMainStack"
+
+#define FTC_UI_UPDATE_HOME_STACK_EVENT          "Ftc::Ui::UpdateHomeStack"
+
+#define FTC_UI_UPDATE_DETAIL_RECV_STACK_EVENT   "Ftc::Ui::UpdateDetailRecvStack"
+
+#define FTC_UI_APPLICATION_QUIT                 "Ftc::Ui::AppicationQuit"
+
+#endif /* _FTC_UI_EVENT_H_ */
---
+++ src/src/ui/FtcEventCallback.h
@@ -0,0 +1,5 @@
+#ifndef _FTC_UI_EVENT_CALLBACK_H_
+#define _FTC_UI_EVENT_CALLBACK_H_
+bool ftc_event_callback_init(); 
+bool ftc_event_callback_dispose(); 
+#endif
