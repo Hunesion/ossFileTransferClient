ff1e894c4dd1e38cb502169dbc12036c9910191b
kjtae <kjtae@hunesion.com>
2020-08-28 15:50:29 +0900

    add send history filelist sub page

---
+++ src/src/ui/ftcdetailsendhistoryfilelistsubpage.h
@@ -0,0 +1,27 @@
+#ifndef _FTC_UI_DETAIL_SEND_HISTORY_FILE_LIST_SUB_PAGE_H_
+#define _FTC_UI_DETAIL_SEND_HISTORY_FILE_LIST_SUB_PAGE_H_
+#include "ftcuicommon.h"
+
+
+
+
+#define FTC_DETAIL_SEND_HISTORY_FILE_LIST_SUB_PAGE_TYPE (ftc_detail_send_history_file_list_sub_page_get_type ())
+G_DECLARE_FINAL_TYPE (FtcDetailSendHistoryFileListSubPage, ftc_detail_send_history_file_list_sub_page, FTC, DETAIL_SEND_HISTORY_FILE_LIST_SUB_PAGE, GtkBox)
+
+
+
+FtcDetailSendHistoryFileListSubPage*      ftc_detail_send_history_file_list_sub_page_new();
+
+void                               ftc_detail_send_history_file_list_sub_page_set_data(FtcDetailSendHistoryFileListSubPage *page, Ftc::Core::GlobalStruct::FtcRequest *ftc_request, RequestDownloadState *request_down_state);
+
+void                               ftc_detail_send_history_file_list_sub_page_set_all_check_active(FtcDetailSendHistoryFileListSubPage *page, bool check);
+
+GSList*                            ftc_detail_send_history_file_list_sub_page_get_checked_file_uid(FtcDetailSendHistoryFileListSubPage *page);
+
+GSList*                            ftc_detail_send_history_file_list_sub_page_get_all_file_uid(FtcDetailSendHistoryFileListSubPage *page);
+
+void                               ftc_detail_send_history_file_list_sub_page_update_activation_view(FtcDetailSendHistoryFileListSubPage *page);
+
+void                               ftc_detail_send_history_file_list_sub_page_set_progress_value(FtcDetailSendHistoryFileListSubPage *page, const char *request_info_file_list_uid, int progress);
+
+#endif /* _FTC_UI_DETAIL_SEND_HISTORY_FILE_LIST_SUB_PAGE_H_ */
